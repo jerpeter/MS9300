fa33a53e1010313b5e369fbafe601967f0de3d8c
jerpeter <jerpeter@yahoo.com>
2024-06-18 02:31:45 -0500

    Added descriptors for MSC device only Updates to several of the label descriptors Added directive logic to allow easy swap between Composite, CDC-ACM only and MSC only

--- descriptors.h
+++ descriptors.h
@@ -30,13 +30,17 @@
  * ownership rights.
  *
  ******************************************************************************/
+#include "Common.h"
 
-#ifndef EXAMPLES_MAX32650_USB_COMPOSITEDEVICE_MSC_CDC_DESCRIPTORS_H_
-#define EXAMPLES_MAX32650_USB_COMPOSITEDEVICE_MSC_CDC_DESCRIPTORS_H_
+//-------------------------------------------------------------------------
+#if USB_COMPOSITE_OPTION /* USB Descriptors for Composite MSC + CDC-ACM */
+//-------------------------------------------------------------------------
+
+#ifndef MAX32650_USB_COMPOSITEDEVICE_MSC_CDC_DESCRIPTORS_H_
+#define MAX32650_USB_COMPOSITEDEVICE_MSC_CDC_DESCRIPTORS_H_
 
 #include <stdint.h>
 #include "usb.h"
-#include "hid_kbd.h"
 
 MXC_USB_device_descriptor_t __attribute__((aligned(4))) composite_device_descriptor = {
     0x12, /* bLength                           */
@@ -428,7 +432,7 @@ __attribute__((aligned(4))) uint8_t mfg_id_desc[] = {
 __attribute__((aligned(4))) uint8_t prod_id_desc[] = {
     0x34, /* bLength */
     0x03, /* bDescriptorType */
-    'M',  0,   'A', 0,   'X', 0,   '3', 0,   '2', 0,   '6', 0,   '6', 0,   '5', 0,   ' ',
+    'M',  0,   'A', 0,   'X', 0,   '3', 0,   '2', 0,   '6', 0,   '5', 0,   '0', 0,   ' ',
     0,    'C', 0,   'o', 0,   'm', 0,   'p', 0,   'o', 0,   's', 0,   'i', 0,   't', 0,
     'e',  0,   ' ', 0,   'D', 0,   'e', 0,   'v', 0,   'i', 0,   'c', 0,   'e', 0,
 };
@@ -441,27 +445,30 @@ __attribute__((aligned(4))) uint8_t serial_id_desc[] = { 0x14, /* bLength */
 __attribute__((aligned(4))) uint8_t cdcacm_func_desc[] = {
     0x20, /* bLength */
     0x03, /* bDescriptorType */
-    'M',  0,   'A', 0,   'X', 0,   '3', 0,   '2', 0,   '6', 0,   '6', 0,   '5',
+    'M',  0,   'A', 0,   'X', 0,   '3', 0,   '2', 0,   '6', 0,   '5', 0,   '0',
     0,    ' ', 0,   'C', 0,   'D', 0,   'C', 0,   'A', 0,   'C', 0,   'M', 0,
 };
 
 __attribute__((aligned(4))) uint8_t msc_func_desc[] = {
     0x3A, /* bLength */
     0x03, /* bDescriptorType */
-    'M',  0,   'A', 0,   'X', 0,   '3', 0,   '2', 0,   '6', 0,   '6', 0,   '5', 0,   ' ', 0,   'M',
+    'M',  0,   'A', 0,   'X', 0,   '3', 0,   '2', 0,   '6', 0,   '5', 0,   '0', 0,   ' ', 0,   'M',
     0,    'a', 0,   's', 0,   's', 0,   ' ', 0,   'S', 0,   't', 0,   'o', 0,   'r', 0,   'a', 0,
     'g',  0,   'e', 0,   ' ', 0,   'D', 0,   'e', 0,   'v', 0,   'i', 0,   'c', 0,   'e', 0,
 };
 
-#endif // EXAMPLES_MAX32650_USB_COMPOSITEDEVICE_MSC_CDC_DESCRIPTORS_H_
+#endif // MAX32650_USB_COMPOSITEDEVICE_MSC_CDC_DESCRIPTORS_H_
 
-#ifndef EXAMPLES_MAX32650_USB_USB_CDCACM_DESCRIPTORS_H_
-#define EXAMPLES_MAX32650_USB_USB_CDCACM_DESCRIPTORS_H_
+//-------------------------------------------------------------------------
+#elif USB_CDC_ACM_ONLY_OPTION /* USB Descriptors for CDC-ACM only */
+//-------------------------------------------------------------------------
+
+#ifndef MAX32650_USB_USB_CDCACM_DESCRIPTORS_H_
+#define MAX32650_USB_USB_CDCACM_DESCRIPTORS_H_
 
 /* **** Includes **** */
 #include <stdint.h>
 #include "usb.h"
-#include "hid_kbd.h"
 
 MXC_USB_device_descriptor_t __attribute__((aligned(4))) device_descriptor = {
     0x12, /* bLength = 18                     */
@@ -746,4 +753,168 @@ __attribute__((aligned(4))) uint8_t cdcacm_func_desc_cdcacm[] = {
     0,    ' ', 0,   'C', 0,   'D', 0,   'C', 0,   'A', 0,   'C', 0,   'M', 0,
 };
 
-#endif // EXAMPLES_MAX32650_USB_USB_CDCACM_DESCRIPTORS_H_
+#endif // MAX32650_USB_USB_CDCACM_DESCRIPTORS_H_
+
+//-------------------------------------------------------------------------
+#elif USB_MSC_ONLY_OPTION /* USB Descriptors for MSC only */
+//-------------------------------------------------------------------------
+
+#ifndef MAX32650_USB_USB_MASSSTORAGE_DESCRIPTORS_H_
+#define MAX32650_USB_USB_MASSSTORAGE_DESCRIPTORS_H_
+
+#include <stdint.h>
+#include "usb.h"
+
+MXC_USB_device_descriptor_t __attribute__((aligned(4))) device_descriptor = {
+    0x12, /* bLength = 18                     */
+    0x01, /* bDescriptorType = Device         */
+    0x0200, /* bcdUSB USB spec rev (BCD)        */
+    0x00, /* bDeviceClass = Unspecified       */
+    0x00, /* bDeviceSubClass                  */
+    0x00, /* bDeviceProtocol                  */
+    0x40, /* bMaxPacketSize0 is 64 bytes      */
+    0x0B6A, /* idVendor (Maxim Integrated)      */
+    0x4402, /* idProduct                        */
+    0x0100, /* bcdDevice                        */
+    0x01, /* iManufacturer Descriptor ID      */
+    0x02, /* iProduct Descriptor ID           */
+    0x03, /* iSerialNumber = (0) No string    */
+    0x01 /* bNumConfigurations               */
+};
+
+/* Device qualifier needed for high-speed operation */
+MXC_USB_device_qualifier_descriptor_t __attribute__((aligned(4))) device_qualifier_descriptor = {
+    0x0A, /* bLength = 10                       */
+    0x06, /* bDescriptorType = Device Qualifier */
+    0x0200, /* bcdUSB USB spec rev (BCD)          */
+    0x00, /* bDeviceClass = Unspecified         */
+    0x00, /* bDeviceSubClass                    */
+    0x00, /* bDeviceProtocol                    */
+    0x40, /* bMaxPacketSize0 is 64 bytes        */
+    0x01, /* bNumConfigurations                 */
+    0x00 /* Reserved, must be 0                */
+};
+
+__attribute__((aligned(4))) struct __attribute__((packed)) {
+    MXC_USB_configuration_descriptor_t config_descriptor;
+    /* Interface #0 Mass Storage Device */
+    MXC_USB_interface_descriptor_t msc_interface_descriptor;
+    MXC_USB_endpoint_descriptor_t endpoint_descriptor_1;
+    MXC_USB_endpoint_descriptor_t endpoint_descriptor_2;
+} config_descriptor = { {
+                            0x09, /*  bLength = 9                     */
+                            0x02, /*  bDescriptorType = Config (2)    */
+                            0x0020, /*  wTotalLength(L/H)               */
+                            0x01, /*  bNumInterfaces                  */
+                            0x01, /*  bConfigValue                    */
+                            0x00, /*  iConfiguration                  */
+                            0xC0, /*  bmAttributes (self-powered, no remote wakeup) */
+                            0x32, /*  MaxPower is 100ma (units are 2ma/bit) */
+                        },
+                        {
+                            /*  First Interface Descriptor For MSC Interface */
+                            0x09, /*  bLength = 9                     */
+                            0x04, /*  bDescriptorType = Interface (4) */
+                            0x00, /*  bInterfaceNumber                */
+                            0x00, /*  bAlternateSetting               */
+                            0x02, /*  bNumEndpoints (one for INm one for OUT)     */
+                            0x08, /*  bInterfaceClass = Mass Storage (8) */
+                            0x06, /*  bInterfaceSubClass = SCSI Transparent Command Set */
+                            0x50, /*  bInterfaceProtocol = Bulk-Only Transport */
+                            0x00, /*  iInterface                      */
+                        },
+                        {
+                            /*  OUT Endpoint 1 (Descriptor #1) */
+                            0x07, /*  bLength                          */
+                            0x05, /*  bDescriptorType (Endpoint)       */
+                            0x01, /*  bEndpointAddress (EP1-OUT)       */
+                            0x02, /*  bmAttributes (bulk)              */
+                            0x0040, /*  wMaxPacketSize                   */
+                            0x00, /*  bInterval (N/A)                  */
+                        },
+                        {
+                            /*  IN Endpoint 2 (Descriptor #2) */
+                            0x07, /*  bLength                          */
+                            0x05, /*  bDescriptorType (Endpoint)       */
+                            0x82, /*  bEndpointAddress (EP2-IN)        */
+                            0x02, /*  bmAttributes (bulk)              */
+                            0x0040, /*  wMaxPacketSize                   */
+                            0x00 /*  bInterval (N/A)                  */
+                        } };
+
+__attribute__((aligned(4))) struct __attribute__((packed)) {
+    MXC_USB_configuration_descriptor_t config_descriptor;
+    /* Interface #0 Mass Storage Device */
+    MXC_USB_interface_descriptor_t msc_interface_descriptor;
+    MXC_USB_endpoint_descriptor_t endpoint_descriptor_1;
+    MXC_USB_endpoint_descriptor_t endpoint_descriptor_2;
+} config_descriptor_hs = { {
+                               0x09, /*  bLength = 9                     */
+                               0x02, /*  bDescriptorType = Config (2)    */
+                               0x0020, /*  wTotalLength(L/H)               */
+                               0x01, /*  bNumInterfaces                  */
+                               0x01, /*  bConfigValue                    */
+                               0x00, /*  iConfiguration                  */
+                               0xC0, /*  bmAttributes (self-powered, no remote wakeup) */
+                               0x32, /*  MaxPower is 100ma (units are 2ma/bit) */
+                           },
+                           {
+                               /*  First Interface Descriptor For MSC Interface */
+                               0x09, /*  bLength = 9                     */
+                               0x04, /*  bDescriptorType = Interface (4) */
+                               0x00, /*  bInterfaceNumber                */
+                               0x00, /*  bAlternateSetting               */
+                               0x02, /*  bNumEndpoints (one for INm one for OUT)     */
+                               0x08, /*  bInterfaceClass = Mass Storage (8) */
+                               0x06, /*  bInterfaceSubClass = SCSI Transparent Command Set */
+                               0x50, /*  bInterfaceProtocol = Bulk-Only Transport */
+                               0x00, /*  iInterface                      */
+                           },
+                           {
+                               /*  OUT Endpoint 1 (Descriptor #1) */
+                               0x07, /*  bLength                          */
+                               0x05, /*  bDescriptorType (Endpoint)       */
+                               0x01, /*  bEndpointAddress (EP1-OUT)       */
+                               0x02, /*  bmAttributes (bulk)              */
+                               0x0200, /*  wMaxPacketSize                   */
+                               0x01, /*  bInterval                        */
+                           },
+                           {
+                               /*  IN Endpoint 2 (Descriptor #2) */
+                               0x07, /*  bLength                          */
+                               0x05, /*  bDescriptorType (Endpoint)       */
+                               0x82, /*  bEndpointAddress (EP2-IN)        */
+                               0x02, /*  bmAttributes (bulk)              */
+                               0x0200, /*  wMaxPacketSize                   */
+                               0x01 /*  bInterval                        */
+                           } };
+
+__attribute__((aligned(4))) uint8_t lang_id_desc_msc[] = {
+    0x04, /* bLength */
+    0x03, /* bDescriptorType */
+    0x09, 0x04 /* bString = wLANGID (see usb_20.pdf 9.6.7 String) */
+};
+
+__attribute__((aligned(4))) uint8_t mfg_id_desc_msc[] = {
+    0x22, /* bLength */
+    0x03, /* bDescriptorType */
+    'M', 0, 'a', 0, 'x', 0, 'i', 0, 'm', 0, ' ', 0, 'I', 0, 'n', 0,
+    't', 0, 'e', 0, 'g', 0, 'r', 0, 'a', 0, 't', 0, 'e', 0, 'd', 0
+};
+
+__attribute__((aligned(4))) uint8_t prod_id_desc_msc[] = {
+    0x28, /* bLength */
+    0x03, /* bDescriptorType */
+    'M', 0, 'A', 0, 'X', 0, 'U', 0, 'S', 0, 'B', 0, ' ', 0, 'M', 0, 'a', 0, 's', 0, 's', 0, ' ', 0, 
+    'S', 0, 't', 0, 'o', 0, 'r', 0, 'a', 0, 'g', 0, 'e', 0
+};
+
+/* Not currently used (see device descriptor), but could be enabled if desired */
+__attribute__((aligned(4)))
+uint8_t serial_id_desc_msc[] = { 26, /* bLength */
+                             0x03, /* bDescriptorType */
+                             '0',  0, '0', 0, '0', 0, '0', 0, '0', 0, '0', 0,
+                             '0',  0, '0', 0, '0', 0, '0', 0, '0', 0, '1', 0 };
+
+#endif // MAX32650_USB_USB_MASSSTORAGE_DESCRIPTORS_H_
+#endif
