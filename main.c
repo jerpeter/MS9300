95d836271de9fe6c15e002feaed24efc2e3d9b86
jerpeter <jerpeter@yahoo.com>
2025-04-07 00:37:22 -0500

    Added reporting of incoming Cell uart0 and uart1 data to the debug port Added clearing uart0 and uart1 Rx buffers and flags

--- main.c
+++ main.c
@@ -609,6 +609,25 @@ static uint8_t s_eventCount = 0;
 		g_powerButtonAction = 0;
 	}
 #endif
+#if 1 /* Test */
+extern uint8_t uart0BufferFull;
+extern uint32_t uart0BufferCount;
+extern uint8_t uart1BufferFull;
+extern uint32_t uart1BufferCount;
+		if (uart0BufferFull)
+		{
+			UNUSED(uart0BufferCount); debugRaw("<Cell: %s> ", (char*)&g_spareBuffer[2048]);
+			memset(&g_spareBuffer[2048], 0, 2048);
+			uart0BufferFull = NO;
+		}
+
+		if (uart1BufferFull)
+		{
+			debugRaw("<%s> (U1, %d chars)\r\n", (char*)&g_spareBuffer[4096], uart1BufferCount);
+			memset(&g_spareBuffer[4096], 0, 2048);
+			uart0BufferFull = NO;
+		}
+#endif
 }
 
 ///----------------------------------------------------------------------------
